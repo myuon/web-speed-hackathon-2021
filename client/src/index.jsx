13495c19ea16431022a61cc4c2ea3c55840bc931
myuon <ioi.joi.koi.loi@gmail.com>
2021-12-10 01:16:10 +0900

    chore: tree shaking for font-awesome

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,21 +8,19 @@ import 'core-js/stable';
 import { AppContainer } from './containers/AppContainer';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faBalanceScale,
-  faEdit,
-  faUser,
-  faSignInAlt,
-  faHome,
-  faPause,
-  faPlay,
-  faExclamationCircle,
-  faCircleNotch,
-  faImages,
-  faMusic,
-  faVideo,
-} from '@fortawesome/free-solid-svg-icons';
-import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faBalanceScale } from '@fortawesome/free-solid-svg-icons/faBalanceScale';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons/faSignInAlt';
+import { faHome } from '@fortawesome/free-solid-svg-icons/faHome';
+import { faPause } from '@fortawesome/free-solid-svg-icons/faPause';
+import { faPlay } from '@fortawesome/free-solid-svg-icons/faPlay';
+import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons/faExclamationCircle';
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons/faCircleNotch';
+import { faImages } from '@fortawesome/free-solid-svg-icons/faImages';
+import { faMusic } from '@fortawesome/free-solid-svg-icons/faMusic';
+import { faVideo } from '@fortawesome/free-solid-svg-icons/faVideo';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons/faCalendarAlt';
 
 library.add(
   faBalanceScale,
